b1bc2dba7a3719d80ce9a20a7a7167b0e5bf21ff
AtiLoik <32639716+AtiLoik@users.noreply.github.com>
2017-10-10 11:37:56 +0200

    MAJ Album d'un artiste

--- vues/v_albumsPourArtiste.php
+++ vues/v_albumsPourArtiste.php
@@ -3,5 +3,5 @@
 		<div class="box">
 			<?php
 			echo "<h2>Les albums de ".$Artiste->getNom()."</h2>";
-			include("/vues/v_albums.php");//puis on affiche la vue qui utilise les données
+			include("vues/v_albums.php");//puis on affiche la vue qui utilise les données
 			?>
